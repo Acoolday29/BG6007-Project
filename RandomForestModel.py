337ffab6646921da746a583cc17d9aa72b9be254
Acoolday29 <gu0002ru@e.ntu.edu.sg>
2025-10-22 22:44:14 +0800

    Update RandomForestModel.py

--- RandomForestModel.py
+++ RandomForestModel.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 # Load cleaned dataset
