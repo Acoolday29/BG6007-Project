951f88ac98b683b75c1ec1755919519f246862b2
Acoolday29 <gu0002ru@e.ntu.edu.sg>
2025-10-22 14:02:37 +0800

    Implement data loading and feature preparation functions
    Added functions to load data and prepare features and labels for training a Random Forest model.

--- RandomForestModel.py
+++ RandomForestModel.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import numpy as np
+from sklearn.ensemble import RandomForestRegressor
 
+# Load cleaned dataset
 def load_cleaned_data():
   print("This is data after cleaning:")
   df = pd.read_csv('cleaned_data.csv')
 
   return df
 
+# Define feature as RGB, labels as SpO2
 def features_and_labels(df):
   features = df[['R', 'G', 'B']].values
+  labels = df['corresponding_SpO2'].values
+
+  return features, labels
+
+# Train random forest model
+def train_RF(x_train, y_train, x_test, y_test):
