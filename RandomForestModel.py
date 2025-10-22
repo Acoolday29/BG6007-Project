d23571c70c70c9f594a2c7699b219d6b2835be3f
Acoolday29 <gu0002ru@e.ntu.edu.sg>
2025-10-22 13:57:53 +0800

    Update RandomForestModel.py

--- RandomForestModel.py
+++ RandomForestModel.py
@@ -3,4 +3,9 @@ import numpy as np
 
 def load_cleaned_data():
   print("This is data after cleaning:")
-  df = pd.read_csv()
+  df = pd.read_csv('cleaned_data.csv')
+
+  return df
+
+def features_and_labels(df):
+  features = df[['R', 'G', 'B']].values
